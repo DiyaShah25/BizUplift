0fe3b804fd0db885f25c329884c4a903a36779f9
DiyaShah25 <shahdiya251004@gmail.com>
2026-04-23 13:51:14 +0530

    Cleaned up redundant nested project folders

--- BizUplift-main/BizUplift-backend/src/routes/negotiationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getNegotiation, saveNegotiation } = require('../controllers/negotiationController');
-const { protect } = require('../middleware/auth');
-
-router.get('/:productId', protect, getNegotiation);
-router.post('/', protect, saveNegotiation);
-
-module.exports = router;
--- BizUplift-main/BizUplift-backend/src/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProductReviews, addReview } = require('../controllers/reviewController');
-const { protect } = require('../middleware/auth');
-
-router.get('/product/:productId', getProductReviews);
-router.post('/', protect, addReview);
-
-module.exports = router;
